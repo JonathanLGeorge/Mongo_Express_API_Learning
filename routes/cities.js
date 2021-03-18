4b3babd91cd6e71115c4709822096d38a80e2dac
Jonathan <jonathan.george.dev@gmail.com>
2021-03-17 21:55:46 -0700

    added some update and add features

--- routes/cities.js
+++ routes/cities.js
@@ -3,10 +3,42 @@ const City = require("../models/City");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json({
-    confirmation: "sucess",
-    data: "this is the cities route",
-  });
+  //  res.json({
+  //    confirmation: "sucess",
+  //    data: "this is the countries route",
+  //  });
+
+  const query = req.query; //what this allows http://localhost:5000/countries?continent=asia
+
+  City.find(query) //http://localhost:5000/countries?continent=asia
+    .then((cities) => {
+      res.json({
+        confirmation: "success",
+        data: cities,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: err.message,
+      });
+    });
 });
 
+//to get by id
+router.get("/:id", (req, res, next) => {
+  City.findById(req.params.id)
+    .then((city) => {
+      res.json({
+        confirmation: "success",
+        data: city,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: "City (" + req.params.id + ") not found",
+      });
+    });
+});
 module.exports = router;
--- routes/countries.js
+++ routes/countries.js
@@ -11,10 +11,10 @@ router.get("/", (req, res, next) => {
   const query = req.query; //what this allows http://localhost:5000/countries?continent=asia
 
   Country.find(query) //http://localhost:5000/countries?continent=asia
-    .then((countries) => {
+    .then((Country) => {
       res.json({
         confirmation: "success",
-        data: countries,
+        data: Country,
       });
     })
     .catch((err) => {
@@ -25,4 +25,70 @@ router.get("/", (req, res, next) => {
     });
 });
 
+//add/?name=nameOfAddedCountry&continent=nameOfContinent&population=number
+router.get("/add", (req, res, next) => {
+  const details = req.query;
+  res.json({
+    confirmation: "success",
+    data: details,
+  });
+
+  Country.create(details)
+    .then((country) => {
+      res.json({
+        confirmation: "success",
+        data: country,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "failed",
+        message: err.message,
+      });
+    });
+});
+
+//update
+//localhost:5000/countries/update/idOfCountry?shemaYouwantUpdated=updatedInfo
+//localhost:5000/countries/update/605291681c6c71d44de159d2?population=54
+//this is a mongoose thing. not a CLI thing
+router.get("/update/:id", (req, res, next) => {
+  const updateDetails = req.query;
+  const countryId = req.params.id;
+  // res.json({
+  //   confirmation: "success",
+  //   data: details,
+  // });
+
+  Country.findByIdAndUpdate(countryId, updateDetails, { new: true })
+    .then((country) => {
+      res.json({
+        confirmation: "success",
+        data: country,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "failed",
+        message: err.message,
+      });
+    });
+});
+
+//to get by id
+router.get("/:id", (req, res, next) => {
+  Country.findById(req.params.id)
+    .then((Country) => {
+      res.json({
+        confirmation: "success",
+        data: Country,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        confirmation: "fail",
+        message: "Country (" + req.params.id + ") not found",
+      });
+    });
+});
 module.exports = router;
